0e0aa9f8a77f1712defb6938267c4d57914a488a
Fabian Alfonso <fabian.alfons@pevaar.com>
2020-11-26 21:46:50 -0500

    Adding search box to Songs

--- src/Components/Songs/MainSongs.js
+++ src/Components/Songs/MainSongs.js
@@ -9,45 +9,10 @@ function MainSongs(){
     const [popularSongs,setPopularSongs] = useState([])
     const [likedSongs,setLikedSongs] = useState([])
     const [favoriteSongs,setFavoriteSongs] = useState([])
+    const [searchedSongs,setSearchedSongs] = useState([])
+
 
     useEffect(() => {
-        const songsAux = [
-            {
-                "song_id": 1,
-                "song_artist":"Masluma",
-                "song_title":"Piece of shit"
-            },
-            {
-                "song_id": 2,
-                "song_artist":"Masluma",
-                "song_title":"Piece of shit"
-            },
-            {
-                "song_id": 3,
-                "song_artist":"Masluma",
-                "song_title":"Piece of shit"
-            },
-            {
-                "song_id": 4,
-                "song_artist":"Masluma",
-                "song_title":"Piece of shit"
-            },
-            {
-                "song_id": 5,
-                "song_artist":"Masluma",
-                "song_title":"Piece of shit"
-            },
-            {
-                "song_id": 6,
-                "song_artist":"Masluma",
-                "song_title":"Piece of shit"
-            },
-            {
-                "song_id": 7,
-                "song_artist":"Masluma",
-                "song_title":"Piece of shit"
-            }
-        ]
         getFavoriteSongs()
         getPopularSongs()
         getLikedSongs()
@@ -63,6 +28,23 @@ function MainSongs(){
         .catch(err => {
             console.log(err)
         })
+    }
+
+    const searchSongs = (text) => {
+        if(text.target.value !== ""){
+            axios.post(process.env.REACT_APP_API_URL + '/search/song/',{
+                search: text.target.value
+            })
+            .then(res => {
+                setSearchedSongs(res.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        }else{
+            setSearchedSongs([])
+        }
+        
     }
 
     const getPopularSongs = () => {
@@ -89,6 +71,32 @@ function MainSongs(){
 
     return(
         <div className="container mt-5">
+
+            <div className="row mt-5">
+                <h1>Search</h1>
+                <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon1" onChange={ (event) => searchSongs(event)} />
+            </div>
+            <div className="row mt-3">
+            {searchedSongs !== undefined ?
+                searchedSongs.map((song) => { return (
+                    <div key={song.song_id} className="col-md-3 mt-3">
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">{song.song_title}</h5>
+                                <h6 className="card-subtitle mb-2 text-muted">{song.song_artist}</h6>
+                                <Link to={"/songs/"+song.song_id}>Details</Link>
+                            </div>
+                        </div>
+                    </div>
+                );
+                })
+                : 
+                <div class="spinner-border" role="status">
+                    <span class="sr-only">Loading...</span>
+                </div>
+            }
+            </div>
+
             <div className="row mt-5">
                 <h1>Recommended for you</h1>
             </div>
