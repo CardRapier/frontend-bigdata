ff8d73428d3726960d7a166727a945a1a3a3ad12
CardRapier <cardrapier@hotmail.com>
2020-11-27 19:30:46 -0500

    Working movies

--- src/Components/Movies/MainMovies.js
+++ src/Components/Movies/MainMovies.js
@@ -1,146 +1,161 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
 
-function MainMovies(){
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-    const [popularMovies,setPopularMovies] = useState([])
-    const [likedMovies,setLikedMovies] = useState([])
-    const [favoriteMovies,setFavoriteMovies] = useState([])
+function MainMovies() {
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [likedMovies, setLikedMovies] = useState([]);
+  const [favoriteMovies, setFavoriteMovies] = useState([]);
+  const [searchedMovies, setSearchedMovies] = useState([]);
 
-    useEffect(() => {
-        const songsAux = [
-            {
-                "song_id": 1,
-                "song_artist":"Masluma",
-                "song_title":"Piece of shit"
-            },
-            {
-                "song_id": 2,
-                "song_artist":"Masluma",
-                "song_title":"Piece of shit"
-            },
-            {
-                "song_id": 3,
-                "song_artist":"Masluma",
-                "song_title":"Piece of shit"
-            },
-            {
-                "song_id": 4,
-                "song_artist":"Masluma",
-                "song_title":"Piece of shit"
-            },
-            {
-                "song_id": 5,
-                "song_artist":"Masluma",
-                "song_title":"Piece of shit"
-            },
-            {
-                "song_id": 6,
-                "song_artist":"Masluma",
-                "song_title":"Piece of shit"
-            },
-            {
-                "song_id": 7,
-                "song_artist":"Masluma",
-                "song_title":"Piece of shit"
-            }
-        ]
-        getFavoriteMovies()
-        getPopularMovies()
-        getLikedMovies()
-        setFavoriteMovies(songsAux)
-        setPopularMovies(songsAux)
-        setLikedMovies(songsAux)
-    },[]);
+  useEffect(() => {
+    getFavoriteMovies();
+    getPopularMovies();
+    getLikedMovies();
+  }, []);
 
-    const getFavoriteMovies = () => {
-        axios.get(process.env.REACT_APP_API_URL + '/recomendation/movies/')
-        .then(res => {
-            setFavoriteMovies(res.data.movies)
+  const getFavoriteMovies = () => {
+    axios
+      .post(process.env.REACT_APP_API_URL + "/recommendation/movies/", {
+        id: localStorage.getItem("id_user"),
+      })
+      .then((res) => {
+        setFavoriteMovies(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const getPopularMovies = () => {
+    axios
+      .get(process.env.REACT_APP_API_URL + "/recommendation/movies/populars/")
+      .then((res) => {
+        setPopularMovies(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const getLikedMovies = () => {
+    axios
+      .get(process.env.REACT_APP_API_URL + "/recommendation/movies/most-liked/")
+      .then((res) => {
+        setLikedMovies(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const searchMovies = (text) => {
+    if (text.target.value !== "") {
+      axios
+        .post(process.env.REACT_APP_API_URL + "/search/movie/", {
+          search: text.target.value,
         })
-        .catch(err => {
-            console.log(err)
+        .then((res) => {
+          setSearchedMovies(res.data);
         })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      setSearchedMovies([]);
     }
+  };
 
-    const getPopularMovies = () => {
-        axios.get(process.env.REACT_APP_API_URL + '/recomendation/movies/popular/')
-        .then(res => {
-            setPopularMovies(res.data.movies)
-        })
-        .catch(err => {
-            console.log(err)
-        })
-    }
+  return (
+    <div className="container mt-5">
+      <div className="row mt-5">
+        <h1>Search</h1>
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Search"
+          aria-label="Search"
+          aria-describedby="basic-addon1"
+          onChange={(event) => searchMovies(event)}
+        />
+      </div>
+      <div className="row mt-3">
+        {searchedMovies !== undefined ? (
+          searchedMovies.map((movie) => {
+            return (
+              <div key={movie.movie_id} className="col-md-3 mt-3">
+                <div className="card">
+                  <div className="card-body">
+                    <h6 className="card-title">{movie.movie_title}</h6>
+                    <Link to={"/movies/" + movie.movie_id}>Details</Link>
+                  </div>
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          <div class="spinner-border" role="status">
+            <span class="sr-only">Loading...</span>
+          </div>
+        )}
+      </div>
 
-    const getLikedMovies = () => {
-        axios.get(process.env.REACT_APP_API_URL + '/recomendation/movies/most-liked/')
-        .then(res => {
-            setLikedMovies(res.data.movies)
-        })
-        .catch(err => {
-            console.log(err)
-        })
-    }
+      <div className="row mt-5">
+        <h1>Recommended for you</h1>
+      </div>
+      <div className="row mt-3">
+        {favoriteMovies.map((movie) => {
+          return (
+            <div key={movie.movie_id} className="col-md-3 mt-3">
+              <div className="card">
+                <div className="card-body">
+                  <h6 className="card-title">{movie.movie_title}</h6>
+                  <Link to={"/movies/" + movie.movie_id}>Details</Link>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
 
-    
+      <div className="row mt-3">
+        <h1>Most liked movies</h1>
+      </div>
+      <div className="row mt-3">
+        {likedMovies.map((movie) => {
+          return (
+            <div key={movie.movie_id} className="col-md-3 mt-3">
+              <div className="card">
+                <div className="card-body">
+                  <h6 className="card-title">{movie.movie_title}</h6>
+                  <Link to={"/movies/" + movie.movie_id}>Details</Link>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
 
-    return(
-        <div className="container mt-5">
-            <div className="row mt-5">
-                <h1>Recommended for you</h1>
+      <div className="row mt-3">
+        <h1>Popular movies</h1>
+      </div>
+      <div className="row mt-3">
+        {popularMovies.map((movie) => {
+          return (
+            <div key={movie.movie_id} className="col-md-3 mt-3">
+              <div className="card">
+                <div className="card-body">
+                  <h6 className="card-title">{movie.movie_title}</h6>
+                  <Link to={"/movies/" + movie.movie_id}>Details</Link>
+                </div>
+              </div>
             </div>
-            <div className="row mt-3">
-                {favoriteMovies.map((song) => { return (
-                    <div key={song.song_id} className="col-md-3 mt-3">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">{song.song_title}</h5>
-                                <h6 className="card-subtitle mb-2 text-muted">{song.song_artist}</h6>
-                            </div>
-                        </div>
-                    </div>
-                );
-                })}
-            </div>
-
-            <div className="row mt-3">
-                <h1>Most liked movies</h1>
-            </div>
-            <div className="row mt-3">
-                {likedMovies.map((song) => { return (
-                    <div key={song.song_id} className="col-md-3 mt-3">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">{song.song_title}</h5>
-                                <h6 className="card-subtitle mb-2 text-muted">{song.song_artist}</h6>
-                                <p>Details</p>
-                            </div>
-                        </div>
-                    </div>
-                );
-                })}
-            </div>
-
-            <div className="row mt-3">
-                <h1>Popular movies</h1>
-            </div>
-            <div className="row mt-3">
-                {popularMovies.map((song) => { return (
-                    <div key={song.song_id} className="col-md-3 mt-3">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">{song.song_title}</h5>
-                                <h6 className="card-subtitle mb-2 text-muted">{song.song_artist}</h6>
-                                <p>Details</p>
-                            </div>
-                        </div>
-                    </div>
-                );
-                })}
-            </div>
-        </div>
-    );
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default MainMovies;
--- src/Components/Songs/MainSongs.js
+++ src/Components/Songs/MainSongs.js
@@ -1,166 +1,179 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import {
-    Link
-  } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 
-function MainSongs(){
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-    const [popularSongs,setPopularSongs] = useState([])
-    const [likedSongs,setLikedSongs] = useState([])
-    const [favoriteSongs,setFavoriteSongs] = useState([])
-    const [searchedSongs,setSearchedSongs] = useState([])
+function MainSongs() {
+  const [popularSongs, setPopularSongs] = useState([]);
+  const [likedSongs, setLikedSongs] = useState([]);
+  const [favoriteSongs, setFavoriteSongs] = useState([]);
+  const [searchedSongs, setSearchedSongs] = useState([]);
 
+  useEffect(() => {
+    getFavoriteSongs();
+    getPopularSongs();
+    getLikedSongs();
+  }, []);
 
-    useEffect(() => {
-        getFavoriteSongs()
-        getPopularSongs()
-        getLikedSongs()
-    },[]);
+  const getFavoriteSongs = () => {
+    axios
+      .post(process.env.REACT_APP_API_URL + "/recommendation/songs/", {
+        id: localStorage.getItem("id_user"),
+      })
+      .then((res) => {
+        setFavoriteSongs(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    const getFavoriteSongs = () => {
-        axios.post(process.env.REACT_APP_API_URL + '/recommendation/songs/',{
-            id: localStorage.getItem('id_user')
+  const searchSongs = (text) => {
+    if (text.target.value !== "") {
+      axios
+        .post(process.env.REACT_APP_API_URL + "/search/song/", {
+          search: text.target.value,
         })
-        .then(res => {
-            setFavoriteSongs(res.data)
+        .then((res) => {
+          setSearchedSongs(res.data);
         })
-        .catch(err => {
-            console.log(err)
-        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      setSearchedSongs([]);
     }
+  };
 
-    const searchSongs = (text) => {
-        if(text.target.value !== ""){
-            axios.post(process.env.REACT_APP_API_URL + '/search/song/',{
-                search: text.target.value
-            })
-            .then(res => {
-                setSearchedSongs(res.data)
-            })
-            .catch(err => {
-                console.log(err)
-            })
-        }else{
-            setSearchedSongs([])
-        }
-        
-    }
+  const getPopularSongs = () => {
+    axios
+      .get(process.env.REACT_APP_API_URL + "/recommendation/songs/populars/")
+      .then((res) => {
+        setPopularSongs(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    const getPopularSongs = () => {
-        axios.get(process.env.REACT_APP_API_URL + '/recommendation/songs/populars/')
-        .then(res => {
-            setPopularSongs(res.data)
-        })
-        .catch(err => {
-            console.log(err)
-        })
-    }
+  const getLikedSongs = () => {
+    axios
+      .get(process.env.REACT_APP_API_URL + "/recommendation/songs/most-liked/")
+      .then((res) => {
+        setLikedSongs(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    const getLikedSongs = () => {
-        axios.get(process.env.REACT_APP_API_URL + '/recommendation/songs/most-liked/')
-        .then(res => {
-            setLikedSongs(res.data)
-        })
-        .catch(err => {
-            console.log(err)
-        })
-    }
-
-    
-
-    return(
-        <div className="container mt-5">
-
-            <div className="row mt-5">
-                <h1>Search</h1>
-                <input type="text" class="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon1" onChange={ (event) => searchSongs(event)} />
-            </div>
-            <div className="row mt-3">
-            {searchedSongs !== undefined ?
-                searchedSongs.map((song) => { return (
-                    <div key={song.song_id} className="col-md-3 mt-3">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">{song.song_title}</h5>
-                                <h6 className="card-subtitle mb-2 text-muted">{song.song_artist}</h6>
-                                <Link to={"/songs/"+song.song_id}>Details</Link>
-                            </div>
-                        </div>
-                    </div>
-                );
-                })
-                : 
-                <div class="spinner-border" role="status">
-                    <span class="sr-only">Loading...</span>
+  return (
+    <div className="container mt-5">
+      <div className="row mt-5">
+        <h1>Search</h1>
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Search"
+          aria-label="Search"
+          aria-describedby="basic-addon1"
+          onChange={(event) => searchSongs(event)}
+        />
+      </div>
+      <div className="row mt-3">
+        {searchedSongs !== undefined ? (
+          searchedSongs.map((song) => {
+            return (
+              <div key={song.song_id} className="col-md-3 mt-3">
+                <div className="card">
+                  <div className="card-body">
+                    <h5 className="card-title">{song.song_title}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">
+                      {song.song_artist}
+                    </h6>
+                    <Link to={"/songs/" + song.song_id}>Details</Link>
+                  </div>
                 </div>
-            }
-            </div>
+              </div>
+            );
+          })
+        ) : (
+          <div class="spinner-border" role="status">
+            <span class="sr-only">Loading...</span>
+          </div>
+        )}
+      </div>
 
-            <div className="row mt-5">
-                <h1>Recommended for you</h1>
-            </div>
-            <div className="row mt-3">
-            {favoriteSongs !== undefined ?
-                favoriteSongs.map((song) => { return (
-                    <div key={song.song_id} className="col-md-3 mt-3">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">{song.song_title}</h5>
-                                <h6 className="card-subtitle mb-2 text-muted">{song.song_artist}</h6>
-                                <Link to={"/songs/"+song.song_id}>Details</Link>
-                            </div>
-                        </div>
+      <div className="row mt-5">
+        <h1>Recommended for you</h1>
+      </div>
+      <div className="row mt-3">
+        {favoriteSongs !== undefined
+          ? favoriteSongs.map((song) => {
+              return (
+                <div key={song.song_id} className="col-md-3 mt-3">
+                  <div className="card">
+                    <div className="card-body">
+                      <h5 className="card-title">{song.song_title}</h5>
+                      <h6 className="card-subtitle mb-2 text-muted">
+                        {song.song_artist}
+                      </h6>
+                      <Link to={"/songs/" + song.song_id}>Details</Link>
                     </div>
-                );
-                })
-                : ''
-            }
-            </div>
+                  </div>
+                </div>
+              );
+            })
+          : ""}
+      </div>
 
-            <div className="row mt-3">
-                <h1>Most liked songs</h1>
-            </div>
-            <div className="row mt-3">
-            {likedSongs !== undefined ?
-                likedSongs.map((song) => { return (
-                    <div key={song.song_id} className="col-md-3 mt-3">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">{song.song_title}</h5>
-                                <h6 className="card-subtitle mb-2 text-muted">{song.song_artist}</h6>
-                                <Link to={"/songs/"+song.song_id}>Details</Link>
-                            </div>
-                        </div>
+      <div className="row mt-3">
+        <h1>Most liked songs</h1>
+      </div>
+      <div className="row mt-3">
+        {likedSongs !== undefined
+          ? likedSongs.map((song) => {
+              return (
+                <div key={song.song_id} className="col-md-3 mt-3">
+                  <div className="card">
+                    <div className="card-body">
+                      <h5 className="card-title">{song.song_title}</h5>
+                      <h6 className="card-subtitle mb-2 text-muted">
+                        {song.song_artist}
+                      </h6>
+                      <Link to={"/songs/" + song.song_id}>Details</Link>
                     </div>
-                );
-                })
-            : ''
-            }
-            </div>
+                  </div>
+                </div>
+              );
+            })
+          : ""}
+      </div>
 
-            <div className="row mt-3">
-                <h1>Popular songs</h1>
-            </div>
-            <div className="row mt-3">
-            {popularSongs !== undefined ?
-                popularSongs.map((song) => { return (
-                    <div key={song.song_id} className="col-md-3 mt-3">
-                        <div className="card">
-                            <div className="card-body">
-                                <h5 className="card-title">{song.song_title}</h5>
-                                <h6 className="card-subtitle mb-2 text-muted">{song.song_artist}</h6>
-                                <Link to={"/songs/"+song.song_id}>Details</Link>
-                            </div>
-                        </div>
+      <div className="row mt-3">
+        <h1>Popular songs</h1>
+      </div>
+      <div className="row mt-3">
+        {popularSongs !== undefined
+          ? popularSongs.map((song) => {
+              return (
+                <div key={song.song_id} className="col-md-3 mt-3">
+                  <div className="card">
+                    <div className="card-body">
+                      <h5 className="card-title">{song.song_title}</h5>
+                      <h6 className="card-subtitle mb-2 text-muted">
+                        {song.song_artist}
+                      </h6>
+                      <Link to={"/songs/" + song.song_id}>Details</Link>
                     </div>
-                );
-                })
-                : ''
-            }
-            </div>
-        </div>
-    );
+                  </div>
+                </div>
+              );
+            })
+          : ""}
+      </div>
+    </div>
+  );
 }
 
 export default MainSongs;
